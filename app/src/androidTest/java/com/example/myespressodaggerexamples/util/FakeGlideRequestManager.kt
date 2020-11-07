c85695c5b13ea7fbc323f83db2dc575571ee1f36
seyed mahdi <ssmmhh1382@gmail.com>
2020-11-07 11:30:55 +0100

    08-Glide manager fake

---
+++ app/src/androidTest/java/com/example/myespressodaggerexamples/util/FakeGlideRequestManager.kt
@@ -0,0 +1,15 @@
+package com.example.myespressodaggerexamples.util
+
+import android.widget.ImageView
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class FakeGlideRequestManager
+@Inject
+constructor()
+    : GlideManager {
+    override fun setImage(imageUrl: String, imageView: ImageView) {
+        //do nothing
+    }
+}
