2c550a90ccc3519b82897775a463bf2595e6dc20
seyed mahdi <ssmmhh1382@gmail.com>
2020-11-09 17:33:00 +0100

    17-Accessing activity instance

--- app/src/androidTest/java/com/example/myespressodaggerexamples/ui/ListFragmentIntegrationTest.kt
+++ app/src/androidTest/java/com/example/myespressodaggerexamples/ui/ListFragmentIntegrationTest.kt
@@ -1,7 +1,10 @@
 package com.example.myespressodaggerexamples.ui
 
+import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.platform.app.InstrumentationRegistry
@@ -42,6 +45,36 @@ class ListFragmentIntegrationTest : BaseMainActivityTests() {
         onView(withId(R.id.no_data_textview)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
 
     }
+
+    // if query for categories returns an empty list, the user should still see
+    // the menu item "All"
+    @Test
+    fun isCategoryListEmpty() {
+        //setup
+        val app = getApp()
+        val apiService = configureFakeApiService(
+            blogPostJsonFileName = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesJsonFileName = Constants.EMPTY_LIST,
+            networkDelay = 0,
+            application = app
+        )
+        configureFakeRepository(apiService, app)
+        injectTest(app)
+        //setup activity
+        val scenario = launchActivity<MainActivity>().onActivity { mainActivity ->
+            val toolBar: Toolbar = mainActivity.findViewById(R.id.tool_bar)
+            mainActivity.viewModel.viewState.observe(mainActivity, Observer { viewState ->
+                if (viewState.activeJobCounter.size == 0) {
+                    toolBar.showOverflowMenu()
+                }
+            })
+        }
+        onView(withSubstring("fun")).check(doesNotExist())
+        onView(withSubstring("dogs")).check(doesNotExist())
+        onView(withSubstring("earthporn")).check(doesNotExist())
+        onView(withSubstring("All")).check(matches(isDisplayed()))
+    }
+
 
     private fun getApp(): TestBaseApplication =
         InstrumentationRegistry.getInstrumentation().targetContext.applicationContext as TestBaseApplication
