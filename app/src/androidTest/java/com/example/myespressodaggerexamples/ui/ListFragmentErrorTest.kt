2d644796f4b33471a7dcd3965192c5e1fd19a7e4
seyed mahdi <ssmmhh1382@gmail.com>
2020-11-11 11:01:34 +0100

    22-Testing data retrieval failure

--- app/src/androidTest/java/com/example/myespressodaggerexamples/ui/ListFragmentErrorTest.kt
+++ app/src/androidTest/java/com/example/myespressodaggerexamples/ui/ListFragmentErrorTest.kt
@@ -7,6 +7,7 @@ import androidx.test.espresso.matcher.ViewMatchers.*
 import com.example.myespressodaggerexamples.R
 import com.example.myespressodaggerexamples.TestBaseApplication
 import com.example.myespressodaggerexamples.di.TestAppComponent
+import com.example.myespressodaggerexamples.ui.viewmodel.state.MainStateEvent
 import com.example.myespressodaggerexamples.util.Constants
 import com.example.myespressodaggerexamples.util.EspressoIdlingResource
 import com.example.myespressodaggerexamples.util.EspressoIdlingResourceRule
@@ -51,6 +52,36 @@ class ListFragmentErrorTest : BaseMainActivityTests() {
 
         onView(withText(R.string.text_error)).check(matches(isDisplayed()))
         onView(withSubstring(Constants.NETWORK_ERROR_TIMEOUT)).check(matches(isDisplayed()))
+
+    }
+    @Test
+    fun isErrorDialogShown_emptyBlog() {
+        //setup
+        configureApiServiceAndRepositoryAndDaggerForTesting(
+            blogPostJsonFileName = Constants.SERVER_ERROR_FILENAME,
+            categoriesJsonFileName = Constants.CATEGORIES_DATA_FILENAME,
+            networkDelay = 0,
+        )
+        //setup activity
+        val scenario = launchActivity<MainActivity>()
+
+        onView(withText(R.string.text_error)).check(matches(isDisplayed()))
+        onView(withSubstring(MainStateEvent.GetAllBlogs().errorInfo())).check(matches(isDisplayed()))
+
+    }
+    @Test
+    fun isErrorDialogShown_emptyCategory() {
+        //setup
+        configureApiServiceAndRepositoryAndDaggerForTesting(
+            blogPostJsonFileName = Constants.BLOG_POSTS_DATA_FILENAME,
+            categoriesJsonFileName = Constants.SERVER_ERROR_FILENAME,
+            networkDelay = 0,
+        )
+        //setup activity
+        val scenario = launchActivity<MainActivity>()
+
+        onView(withText(R.string.text_error)).check(matches(isDisplayed()))
+        onView(withSubstring(MainStateEvent.GetCategories().errorInfo())).check(matches(isDisplayed()))
 
     }
 
